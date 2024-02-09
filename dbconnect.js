85ebbe3508b467190dc04f4c63d2b85dcbc27d9f
notxmen <101659866+xmenzzz@users.noreply.github.com>
2024-02-09 18:36:09 +0000

    FINAL

---
+++ dbconnect.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+  host: "mysql",
+  user: "root",
+  port: 3306, 
+  password: "secret",
+  database: "temporadas"
+});
+
+con.connect(function(err) {
+  if (err) {
+    console.log('Error on database connection!!!!! ');
+    throw err;
+}
+console.log('Database connection active. ');
+});
+
+con.on('error', function(err) {
+  console.loh('Database error: ', err);
+} 
+);
+
+
+module.exports = con;
